1747ebdd787dbe01df33b8c57026127f40b2a7fd
‘webfer’ <mx7.bcn@pm.me>
2022-05-18 02:46:06 -0700

    feat: add initial files of the project

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MarketingApp from './components/MarketinApp';
+
+export default () => {
+  return (
+    <div>
+      <h1>Hi there!</h1>;
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Mount function to start up the app
+const mount = (el) => {
+  ReactDOM.render(<App />, el);
+};
+
+// If we are in developtment and insolation,
+// call mount inmediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_marketing-dev-root');
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+// We are running through the containet
+// and we should export the mount function
+export { mount };
