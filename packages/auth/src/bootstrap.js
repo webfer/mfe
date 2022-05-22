f60205f7c800025c4751fca81a806db910072c44
‘webfer’ <mx7.bcn@pm.me>
2022-05-22 11:55:53 -0700

    feat: added auth feature

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { createMemoryHistory, createBrowserHistory } from 'history';
+import ReactDOM from 'react-dom';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
   const history =
     defaultHistory ||
     createMemoryHistory({
       initialEntries: [initialPath],
     });
-
   if (onNavigate) {
     history.listen(onNavigate);
   }
-
-  ReactDOM.render(<App history={history} />, el);
-
+  ReactDOM.render(<App onSignIn={onSignIn} history={history} />, el);
   return {
     onParentNavigate({ pathname: nextPathname }) {
       const { pathname } = history.location;
-
+      console.log(nextPathname);
       if (pathname !== nextPathname) {
         history.push(nextPathname);
       }
@@ -31,8 +28,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 // If we are in developtment and insolation,
 // call mount inmediately
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root');
-
+  const devRoot = document.querySelector('#_auth-dev-root');
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
