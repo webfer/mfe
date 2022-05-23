3bb34a8d34397445fb0ca5c34905118fedf8799b
‘webfer’ <mx7.bcn@pm.me>
2022-05-22 18:36:32 -0700

    feat: add dashboard and Showing the Dashboard

---
+++ packages/dashboard/src/bootstrap.js
@@ -0,0 +1,21 @@
+import { createApp } from 'vue';
+import Dashboard from './components/Dashboard.vue';
+
+// Mount function to start up the app
+const mount = (el) => {
+  const app = createApp(Dashboard);
+  app.mount(el);
+};
+
+// If we are in developtment and insolation,
+// call mount inmediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_dashboard-dev-root');
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+// We are running through the containet
+// and we should export the mount function
+export { mount };
